5c0e6aa7968fbe5f47bfd883d21d33b78be65ea0
Emir <babalik.emir@gmail.com>
2025-04-17 17:22:26 +0300

    ConsoleAppDLL

---
+++ ConsoleApp1/ClassLibrary1/PluginClass.cs
@@ -0,0 +1,70 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class PluginClass
+    {
+
+        public void Run(SqlConnection cnn, string cmd)
+        {
+
+            var parts = cmd.Split(' ', (char)StringSplitOptions.RemoveEmptyEntries);
+            if (parts.Length == 0)
+            {
+                Console.WriteLine("Komut bos.");
+                return;
+            }
+
+
+
+            switch (parts[0].ToLower())
+            {
+                case "insert":
+                    if (parts.Length < 4)
+                    {
+                        Console.WriteLine("Kullanım: insert [Ad] [Soyad] [Yaş]");
+                        return;
+                    }
+                    var insertSql = "INSERT INTO users (FirstName, LastName, Age) VALUES (@FirstName, @LastName, @Age)";
+                    cnn.Execute(insertSql, new { FirstName = parts[1], LastName = parts[2], Age = int.Parse(parts[3]) });
+                    Console.WriteLine("Kullanıcı eklendi.");
+                    break;
+
+                case "delete":
+                    if (parts.Length < 2)
+                    {
+                        Console.WriteLine("Kullanım: delete [Id]");
+                        
+                    }
+                    var deleteSql = "DELETE FROM users WHERE Id = @Id";
+                    cnn.Execute(deleteSql, new { Id = int.Parse(parts[1]) });
+                    
+                    Console.WriteLine("Kullanıcı silindi.");
+                    break;
+
+                case "list":
+                    var users = cnn.Query("SELECT * FROM users");
+                    foreach (var user in users)
+                    {
+                        Console.WriteLine($"{user.Id}: {user.FirstName} {user.LastName}, {user.Age} yaşında");
+                    }
+                    break;
+
+                default:
+                    Console.WriteLine("Bilinmeyen komut");
+                    break;
+            }
+
+            
+        }
+
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var dllPath = "C:\\Users\\Emir\\source\\repos\\ConsoleApp1\\ClassLibrary1\\bin\\Debug\\ClassLibrary1.dll";
+            var connectionString = "Server=DESKTOP-RA4ABQ2\\SQLEXPRESS;Database=Test33;Trusted_Connection=True;";
+
+             var connection = new SqlConnection(connectionString);
+            connection.Open();
+
+            
+            InitializeDatabase(connection);
+
+            Console.WriteLine("Komut gir (insert/delete/list). ): ");
+
+            while (true)
+            {
+                Console.Write(" ");
+                var input = Console.ReadLine();
+                if (input == "exit") break;
+
+                if (!File.Exists(dllPath))
+                {
+                    Console.WriteLine("DLL bulunamadı.");
+                    continue;
+                }
+
+                try
+                {
+                    var a = Assembly.LoadFrom(dllPath);
+                    var type = a.GetType("ClassLibrary1.PluginClass");
+                    var instance = Activator.CreateInstance(type);
+                    var method = type.GetMethod("Run");
+
+                    method.Invoke(instance, new object[] { connection, input });
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Hata: {ex.Message}");
+                }
+            }
+        }
+        private static void InitializeDatabase(SqlConnection connection)
+        {
+            
+        }
+
+    }
+    
+}
